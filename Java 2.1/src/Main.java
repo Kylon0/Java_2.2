50a078d523b0ad9777404b371a98e11b0300e6c2
Kylon0 <71556566+Kylon0@users.noreply.github.com>
2021-12-07 00:23:16 +0300

    Delete Java 2.1 directory

--- Java 2.1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int price = 2050;
-        int bonus = 20;
-        int mile = price/bonus;
-        System.out.println(mile);
-    }
-}
